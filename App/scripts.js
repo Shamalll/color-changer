422d36de8bf5ab038f1b8f747bab87256b7d608a
Shamal Williams <shamal@Shamals-MacBook-Pro.local>
2021-06-16 22:35:15 -0400

    tried changing the button, failed. I'm stuck.

---
+++ App/scripts.js
@@ -0,0 +1,21 @@
+// Don't delete
+function bodyColor(color){
+    document.body.style.background = color;
+}
+
+// If body is called, change color again. 
+// if(document.getElementById('body'))
+
+
+// Don't delete.
+function headingColor(color){
+    document.getElementById("heading").style.background = color;
+}
+
+// if(document.getElementById("button")){
+//     document.getElementById("button").style.background = color;
+// };
+
+// if(bodyColor.onclick){
+//     document.getElementById("button").style.color = 'green';
+// }
